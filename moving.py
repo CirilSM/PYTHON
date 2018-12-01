0bdc86df29b71672fbadfe31e06ba095371edb9e
Ciril S Mandoli <43570110+CirilSM@users.noreply.github.com>
2018-12-01 17:17:31 +0530

    Add files via upload
    Program to create a moving banner in the program (marque in HTML like)

---
+++ moving.py
@@ -0,0 +1,10 @@
+import os
+s=raw_input("Enter the name :")
+os.system("cls")
+for c in range (len(s)+89):
+	print s,
+	for j in range (999999):
+                pass
+	s=" "+s
+	os.system("cls")
+print "thank you"
